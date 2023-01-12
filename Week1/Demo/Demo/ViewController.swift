3cb7be61d20423ed664dbf69b5d2e098340e0c10
Bennett Lee <bennettlee@Bennetts-MacBook-Pro.local>
2023-01-11 18:09:52 -0800

    week 1

---
+++ Week1/Demo/Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Bennett Lee on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
